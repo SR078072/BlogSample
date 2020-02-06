f93569aa2696212ed86a436ec1297871ee0c90b6
Raj,Shashankit <Shashankit.Raj@cerner.com>
2020-02-06 12:15:41 +0530

    CRUD finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 root 'home#index'
 
-resources :articles
+resources :articles do
+  resources :comments
+end
 
 get '/about' => 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
